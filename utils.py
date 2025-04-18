1bdee89836ed69c640fc2c27ad8a790c483e66a3
Andrey-Baranov-24-4 <sportumay6@gmail.com>
2025-04-18 15:23:32 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 import bcrypt
 import asyncio
 
+
 async def hash_password(password: str) -> str:
     return await asyncio.to_thread(
         lambda: bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
     )
 
+
 async def check_password(password: str, hashed: str) -> bool:
     return await asyncio.to_thread(
         lambda: bcrypt.checkpw(password.encode(), hashed.encode())
     )
-
-
