932da402028de5751758bc633ad5f871cf2cac49
Andrey-Baranov-24-4 <sportumay6@gmail.com>
2025-04-18 15:21:18 +0300

    Update db.py
    улучшение синтаксиса

--- db.py
+++ db.py
@@ -1,11 +1,9 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from models import *
 from config import DATABASE_URL
 
-
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
-async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 async def init_db():
