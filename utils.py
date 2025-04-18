b48a3822b036b3de3c93696bb081fd71a399c58b
Andrey-Baranov-24-4 <sportumay6@gmail.com>
2025-04-18 16:38:23 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,13 +2,10 @@ import bcrypt
 import asyncio
 
 
+# Хеширую пароль с использованием алгоритма bcrypt (в отдельном потоке, чтобы не блокировать event loop)
+# Вход: строка с паролем
+# Выход: строку с хешем пароля
 async def hash_password(password: str) -> str:
     return await asyncio.to_thread(
         lambda: bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-    )
-
-
-async def check_password(password: str, hashed: str) -> bool:
-    return await asyncio.to_thread(
-        lambda: bcrypt.checkpw(password.encode(), hashed.encode())
     )
