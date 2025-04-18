d0bb88bf1f688b17556722253fb134c9c3d7cfcb
Andrey-Baranov-24-4 <sportumay6@gmail.com>
2025-04-18 15:24:16 +0300

    Update cors.py
    Улучшение синтаксиса

--- cors.py
+++ cors.py
@@ -3,13 +3,16 @@ from models import User, Request
 from db import async_session_maker
 from utils import hash_password
 
+
 async def user_exists_by_telegram_id(telegram_id: int):
     async with async_session_maker() as session:
         return await session.scalar(select(User).where(User.telegram_id == telegram_id))
 
+
 async def user_exists_by_login(login: str):
     async with async_session_maker() as session:
         return await session.scalar(select(User).where(User.username == login))
+
 
 async def create_user(login: str, password: str, telegram_id: int):
     password_hashed = await hash_password(password)
@@ -18,9 +21,11 @@ async def create_user(login: str, password: str, telegram_id: int):
         session.add(user)
         await session.commit()
 
+
 async def get_user_by_telegram_id(telegram_id: int):
     async with async_session_maker() as session:
         return await session.scalar(select(User).where(User.telegram_id == telegram_id))
+
 
 async def save_simulation_result(user_id: int, content: str):
     async with async_session_maker() as session:
@@ -28,6 +33,8 @@ async def save_simulation_result(user_id: int, content: str):
         session.add(request)
         await session.commit()
 
+
 async def get_user_requests(user_id: int):
     async with async_session_maker() as session:
-        return await session.scalars(select(Request).where(Request.user_id == user_id).order_by(Request.timestamp.desc()))
+        return await session.scalars(
+            select(Request).where(Request.user_id == user_id).order_by(Request.timestamp.desc()))
